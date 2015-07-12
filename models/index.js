6d89f6e8b780c235ec27973118f0d26c63021489
Arka Ganguli <arka.ganguli@shopify.com>
2015-07-11 20:02:08 -0400

    WIP

--- models/index.js
+++ models/index.js
@@ -9,8 +9,8 @@ var config = require(__dirname + '/../config/config.json')[env];
 // initialize database connection
 var sequelize = new Sequelize(config.database, config.username, config.password, config);
 var db = {};
-// var passport = require('passport');
-// var localStrategy = require('passport-local').Strategy;
+var passport = require('passport');
+var localStrategy = require('passport-local').Strategy;
 
 
 fs.readdirSync(__dirname).filter(function(file) {
@@ -26,38 +26,38 @@ Object.keys(db).forEach(function(modelName) {
 	}
 });
 
-// passport.use(new localStrategy({
-//     // set the field name here
-//     first_name: 'firstname',
-//     password: 'password'
-//   },
-//   function(first_name, password, done) {
-//     /* get the username and password from the input arguments of the function */
+passport.use(new localStrategy({
+    // set the field name here
+    first_name: 'firstname',
+    password: 'password'
+  },
+  function(first_name, password, done) {
+    /* get the username and password from the input arguments of the function */
 
-//     // query the user from the database
-//     // don't care the way I query from database, you can use
-//     // any method to query the user from database
-//     User.find( { where: {first_name: first_name}} )
-//       .success(function(user){
+    // query the user from the database
+    // don't care the way I query from database, you can use
+    // any method to query the user from database
+    User.find( { where: {first_name: first_name}} )
+      .success(function(user){
 
-//         if(!user)
-//           // if the user is not exist
-//           return done(null, false, {message: "The user is not exist"});
-//         else if(!hashing.compare(password, user.password))
-//           // if password does not match
-//           return done(null, false, {message: "Wrong password"});
-//         else
-//           // if everything is OK, return null as the error
-//           // and the authenticated user
-//           return done(null, user);
+        if(!user)
+          // if the user is not exist
+          return done(null, false, {message: "The user is not exist"});
+        else if(!hashing.compare(password, user.password))
+          // if password does not match
+          return done(null, false, {message: "Wrong password"});
+        else
+          // if everything is OK, return null as the error
+          // and the authenticated user
+          return done(null, user);
 
-//       })
-//       .error(function(err){
-//         // if command executed with error
-//         return done(err);
-//       });
-//   }
-// ));
+      })
+      .error(function(err){
+        // if command executed with error
+        return done(err);
+      });
+  }
+));
 
 //RELATIONSHIP CODE -- BEGIN
 
