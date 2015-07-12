5e55d7ba1eae09056e990c561ca025e1ed91f989
Arka Ganguli <arka.ganguli@shopify.com>
2015-07-12 19:03:33 -0400

    remove routes that are not needed

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,6 @@
 var express = require('express');
 var router = express.Router();
-//var User =
 var models = require("../models"); //place on top of the file</pre>
-var passport = require('passport');
-var LocalStrategy = require('passport-local').Strategy;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
--- routes/users.js
+++ routes/users.js
@@ -1,68 +1,18 @@
 var express = require('express');
 var router = express.Router();
 var models =  require('../models');
-// var passport = require('passport');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('User route');
 });
 
-// router.get('/user', function(req, res, next) {
-//   models.User.find({
-//     where: {id: }
-//   })
-// });
-
-// router.get('/all', function(req, res) {
-//   // models.User.findAll({
-//   //   include: [ models.Task ]
-//   // }).then(function(users) {
-//   //   res.render('index', {
-//   //     title: 'Express',
-//   //     users: users
-//   //   });
-//   // });
-//   //res.send('respond with a resource, hit the correct node');
-
-//   models.User.findById(1).then(function(project) {
-//   // project will be an instance of Project and stores the content of the table entry
-//   // with id 123. if such an entry is not defined you will get null
-//     res.send(project);
-//   });
-// });
 
 router.get('/all', function(req, res) {
   models.User.findAll().then(function(user) {
     //TODO: Need to find how to properly output JSON response
     res.send(user);
   });
-});
-
-// Serialize sessions
-// passport.serializeUser(function(user, done) {
-//   done(null, user.id);
-// });
-
-// passport.deserializeUser(function(id, done) {
-//   db.User.find({where: {id: id}}).success(function(user){
-//     done(null, user);
-//   }).error(function(err){
-//     done(err, null);
-//   });
-// });
-
-
-router.get('/add', function(req, res) {
-
-  var firstName = req.param('first_name');
-  var lastName = req.param('last_name');
-
-  models.User
-    .findOrCreate({ where : { firstName: firstName, lastName: lastName }, defaults: {}})
-    .spread(function(user, created) {
-      res.send("created");
-    });
 });
 
 module.exports = router;
