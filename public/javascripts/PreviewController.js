08c28e1bbbad952a2337d58d30e25129f5ea3640
k-dang <dangkevin12@gmail.com>
2015-09-30 01:37:16 -0400

    modularized into angular services/factories and inital work on routes

--- public/javascripts/PreviewController.js
+++ public/javascripts/PreviewController.js
@@ -81,6 +81,10 @@ graphModule.controller("PieCtrl", ['$scope',function($scope) {
   $scope.title = 'PieGraph';
   $scope.label = ["Download Sales", "In-Store Sales", "Mail-Order Sales"];
   $scope.data = [300, 500, 100];
+  $scope.testButton = function(Test){
+    $scope.data.push(Test.weightInput);
+    $scope.label.push(Test.testID);
+  };
 }]);
 
 function getIndexed(arr, val){
---
+++ public/javascripts/homeController.js
@@ -0,0 +1,3 @@
+angular.module('app').controller('homeController',['$scope',function($scope) {
+	$scope.message = 'Yes';
+}]);
