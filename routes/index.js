6090007a4781be14e1f856a64819d2246f9d765c
k-dang <dangkevin12@gmail.com>
2015-10-14 00:11:42 -0400

    angular handles all other routes

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var models = require("../models"); //place on top of the file</pre>
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('*', function(req, res, next) {
   // models.User.findAll().then(function(users) {
       // res.render('dashboard',function(err, html){
       	res.sendfile('./public/views/dashboard.html');
