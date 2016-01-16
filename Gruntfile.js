6cd25a80dd822d4760aaf27636429ca7379d89a2
k-dang <dangkevin12@gmail.com>
2016-01-16 08:24:38 -0500

    fix

--- Gruntfile.js
+++ Gruntfile.js
@@ -339,7 +339,7 @@ module.exports = function (grunt) {
     ngtemplates: {
       dist: {
         options: {
-          module: 'edulyticsApp',
+          module: 'myApp',
           htmlmin: '<%= htmlmin.dist.options %>',
           usemin: 'scripts/scripts.js'
         },
